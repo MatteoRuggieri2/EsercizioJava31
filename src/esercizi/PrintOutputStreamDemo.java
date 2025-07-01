6c7dd957e9708f24ee63cda9fe6f695c6f7ba948
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2025-07-01 22:48:02 +0200

    Exercise Published

---
+++ src/esercizi/PrintOutputStreamDemo.java
@@ -0,0 +1,36 @@
+package esercizi;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class PrintOutputStreamDemo {
+
+	public static void main(String[] args) {
+		PrintOutputStreamDemo posd = new PrintOutputStreamDemo();
+		posd.run();
+	}
+
+	private void run() {
+		// Creates a FileOutputStream
+		FileOutputStream file = null;
+		
+		try {
+			file = new FileOutputStream("src/text_files/dataPrint.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// Creates a PrintStream
+		PrintStream output = new PrintStream(file, true);
+		
+		/* Alla fine controllo se ci sono stati errori (chechError ritorna true se sono stati riscontrati errori
+		 * altrimenti torna false) nell'oggetto PrintStream*/
+		if (output.checkError()) {
+			System.out.println("C'è stato un errore!");
+		} else {
+			System.out.println("Operazioni completate correttamente : )");
+		}
+	}
+
+}
